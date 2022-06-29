882590a54527154e15fc97c8824cdf11116fb5eb
paoloBugarin <paolopaolobugarin@gmail.com>
2022-06-28 19:13:01 -0700

    trying to fix cors error, removed app.use(cors())

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const ImageKit = require('imagekit');
 require("dotenv").config();
 app.use(express.json());
-app.use(cors());    //to allow api connection from computer to react project
+// app.use(cors());    //to allow api connection from computer to react project
 const db = require("./models"); //import tables from models folder
 
 // routers
