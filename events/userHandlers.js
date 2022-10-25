edf41978101135ca92cb4dc8b93070125f751886
paoloBugarin <paolopaolobugarin@gmail.com>
2022-10-25 00:37:25 -0700

    updated userHandlers listener --fixed logout listener that wipes out all user's sockets

--- events/userHandlers.js
+++ events/userHandlers.js
@@ -41,7 +41,6 @@ const userHandlers = (io, socket) => {
       })
     } else {
       userFriends = [];
-      // connectedFriends = [];
     }
   })
 
@@ -57,28 +56,43 @@ const userHandlers = (io, socket) => {
 
   socket.on("onLogout", async () => {
     if(io.adapter.connectedUsers && user){
-      let item = io.adapter.connectedUsers.find(item => item.id === user.id)
-      item.sockets && item.sockets.splice(item.sockets.indexOf(socket.id), 1);      //double check this
+      let item = io.adapter.connectedUsers.find(_item => _item.id === user.id)
+      if(item.sockets) item.sockets = [...item.sockets.filter(_item => _item !== socket.id)];
       //if user has no sockets connected, update Users db
       if(item.sockets && item.sockets.length === 0) {
-        io.adapter.connectedUsers.splice(io.adapter.connectedUsers.indexOf(item), 1);
+        io.adapter.connectedUsers = [ ...io.adapter.connectedUsers.filter(_item => _item !== item)]
         await Users.update({isLoggedIn: false}, { where: { id: user.id } });
         //emit to friend's sockets
-        io.adapter.connectedUsers.filter(item => userFriends.includes(item.id)).forEach(item => {
-          item.sockets.forEach(_item => io.of("users").to(_item).emit("friend_logout", user.id))
+        io.adapter.connectedUsers.filter((_item) => userFriends.includes(_item.id)).forEach(__item => {
+          __item.sockets.forEach(____item => io.of("users").to(____item).emit("friend_logout", user.id))
         })
-        user = null;
-        userFriends = null;
       };
-    }   
+    }  
+    user = null;
+    userFriends = null; 
   })
 
 
   socket.on('disconnect', async () => {
+    // if(io.adapter.connectedUsers && user){
+    //   let item = io.adapter.connectedUsers.find(_item => _item.id === user.id)
+    //   if(item.sockets) item.sockets = [...item.sockets.filter(_item => _item !== socket.id)];
+    //   //if user has no sockets connected, update Users db
+    //   if(item.sockets && item.sockets.length === 0) {
+    //     io.adapter.connectedUsers = [ ...io.adapter.connectedUsers.filter(_item => _item !== item)]
+    //     await Users.update({isLoggedIn: false}, { where: { id: user.id } });
+    //     //emit to friend's sockets
+    //     io.adapter.connectedUsers.filter((_item) => userFriends.includes(_item.id)).forEach(__item => {
+    //       __item.sockets.forEach(____item => io.of("users").to(____item).emit("friend_logout", user.id))
+    //     })
+    //   };
+    // }  
+    // user = null;
+    // userFriends = null; 
     if(io.adapter.connectedUsers && user){
       //remove from socket.adapter.connectedUsers array
       let disconnectedUser = io.adapter.connectedUsers.find(item => item.id === user.id)
-      if(disconnectedUser) disconnectedUser.sockets.splice(disconnectedUser.sockets.indexOf(socket.id), 1);
+      if(disconnectedUser) disconnectedUser.sockets = [ ...disconnectedUser.sockets.filter(item => item !== socket.id)];
       
       //remove connected user if no sockets exist
       if(disconnectedUser && disconnectedUser.sockets.length === 0) {
@@ -87,6 +101,7 @@ const userHandlers = (io, socket) => {
       };
     }
   });
+  
 }
   
 module.exports = userHandlers;
